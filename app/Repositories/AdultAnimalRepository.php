3002bc8a7ec6d92cb90ed832b172ab7c98e9d3a1
tousan1118 <kka.saito.devnull@gmail.com>
2023-01-09 23:17:00 +0900

    fix2

---
+++ app/Repositories/AdultAnimalRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Repository;
+
+
+use Illuminate\Support\Facades\DB;
+use App\Interface\Providers\AnimalRepositoryInterface;
+use Domain\Animal\Animal;
+use Domain\Animal\AnimalId;
+
+class AdultAnimalRepository implements AnimalRepositoryInterface
+{
+    /**
+     * @param Animal $animal
+     * @return mixed
+     */
+    public function save(Animal $animal)
+    {
+        DB::table('Animals')
+            ->updateOrInsert(
+                ['id' => $animal->getId()],
+                ['name' => $animal->getName()]
+            );
+    }
+
+    /**
+     * @param AnimalId $id
+     * @return Animal
+     */
+    public function find(AnimalId $id)
+    {
+        $animal = DB::table('Animals')->where('id', $id->getValue())->first();
+
+        return new Animal($id, $animal->name);
+    }
+
+    /**
+     * @param int $page
+     * @param int $size
+     * @return mixed
+     */
+    public function findByPage($page, $size)
+    {
+        // TODO: Implement findByPage() method.
+    }
+}
